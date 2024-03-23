ad093ae354c5be8fc9dc650ea032a2b597a3dab3
Tamer Erdoğan <tamer.erdogaan@gmail.com>
2024-03-23 06:48:13 +0300

    FPController part #2 Code have shake problem.

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -29,5 +29,14 @@ public class FirstPersonController : MonoBehaviour
         Vector2 mouseDelta = playerInput.Player.Look.ReadValue<Vector2>();
         transform.rotation *= Quaternion.Euler(0, mouseDelta.x, 0);
 
+        float desiredAngle = -mouseDelta.y;
+        float nextAngle = Camera.main.transform.eulerAngles.x + desiredAngle;
+        if (nextAngle > 180)
+            nextAngle -= 360;
+
+        if (nextAngle > 30 || nextAngle < -30)
+            return;
+
+        Camera.main.transform.rotation *= Quaternion.Euler(desiredAngle, 0, 0);
     }
 }
