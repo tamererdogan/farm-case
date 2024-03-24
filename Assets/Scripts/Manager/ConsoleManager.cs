cae56a0e3668e11114b9c298411ba339c4633954
Tamer Erdoğan <tamer.erdogaan@gmail.com>
2024-03-24 20:52:45 +0300

    Console system done.

---
+++ Assets/Scripts/Manager/ConsoleManager.cs
@@ -0,0 +1,43 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsoleManager : MonoBehaviour
+{
+    #region SINGLETON
+    public static ConsoleManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+    #endregion SINGLETON
+
+    [SerializeField]
+    private GameObject contentUI;
+
+    [SerializeField]
+    private Scrollbar scrollbar;
+
+    [SerializeField]
+    private GameObject messagePrefab;
+
+    public void AddMessage(string message, Color color = default)
+    {
+        if (color == default)
+            color = Color.white;
+
+        GameObject messageObject = Instantiate(messagePrefab, contentUI.transform);
+        TMP_Text textObject = messageObject.GetComponent<TMP_Text>();
+        textObject.text = message;
+        textObject.color = color;
+
+        scrollbar.value = 0f;
+    }
+}
